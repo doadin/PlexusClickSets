e975caabaab5a8d7481ecfef5ac0d171263e8e2c
tbkizle <tbkizle@gmail.com>
2021-04-23 10:03:02 -0400

    Fix for classic wow

--- PlexusClickSetsFrame.lua
+++ PlexusClickSetsFrame.lua
@@ -1,4 +1,15 @@
 --local L = LibStub("AceLocale-3.0"):GetLocale("PlexusClickSets")
+local function IsClassicWow()
+	return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
+end
+
+local function IsTBCWow()
+	return WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
+end
+
+local function IsRetailWow()
+	return WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
+end
 local i
 local queue = false
 
@@ -19,10 +30,12 @@ local function makeMovable(frame)
 end
 
 function PlexusClickSetsFrame_OnLoad(self)
-    self:RegisterEvent("PLAYER_REGEN_ENABLED");
-    self:RegisterEvent("VARIABLES_LOADED");
-    self:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED");
-    self:RegisterEvent("PLAYER_ALIVE");
+    self:RegisterEvent("PLAYER_REGEN_ENABLED")
+    self:RegisterEvent("VARIABLES_LOADED")
+    if IsRetailWow() then
+        self:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+    end
+    self:RegisterEvent("PLAYER_ALIVE")
     self:SetBackdrop({
         bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
         edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
@@ -31,18 +44,18 @@ function PlexusClickSetsFrame_OnLoad(self)
         tileSize = 32,
         edgeSize = 32,
         insets = { left = 11, right = 11, top = 12, bottom = 10 },
-    });
+    })
 
     makeMovable(self)
-    PanelTemplates_SetNumTabs(self, PLEXUS_CLICK_SETS_BUTTONS + 2);
-    self.selectedTab = 1;
-    PanelTemplates_UpdateTabs(self);
+    PanelTemplates_SetNumTabs(self, PLEXUS_CLICK_SETS_BUTTONS + 2)
+    self.selectedTab = 1
+    PanelTemplates_UpdateTabs(self)
 
     for i = 1, 8 do
         getglobal("PlexusClickSetButton"..i.."Title"):SetText(PlexusClickSets_Titles[i])
     end
 
-    --PlexusClickSetsFrame_TabOnClick();
+    --PlexusClickSetsFrame_TabOnClick()
     table.insert(UISpecialFrames, self:GetName())
 end
 
@@ -67,7 +80,7 @@ function PlexusClickSetsFrame_OnEvent(self, event, arg1)
     end
 
     if(event=="VARIABLES_LOADED" or (event=="PLAYER_SPECIALIZATION_CHANGED" and arg1=="player") or event=="PLAYER_ALIVE") then
-        PlexusClickSetsFrame_UpdateAll(1);
+        PlexusClickSetsFrame_UpdateAll(1)
     end
 end
 
@@ -86,36 +99,36 @@ function PlexusClickSetsFrame_TypeUpdate(id)
     argF:SetBackdropColor(0, 0, 0, .5)
 
     if( value=="spell" or value=="macro" ) then
-        argF:Show();
+        argF:Show()
     else
-        argF:Hide();
+        argF:Hide()
     end
 
     local below = getglobal("PlexusClickSetButton"..(id+1))
     if(below) then
-        below:ClearAllPoints();
+        below:ClearAllPoints()
         if argF:IsVisible() then
-            below:SetPoint("TOPLEFT", argF, "BOTTOMLEFT", -50, 0);
+            below:SetPoint("TOPLEFT", argF, "BOTTOMLEFT", -50, 0)
         else
-            below:SetPoint("TOPLEFT", getglobal("PlexusClickSetButton"..id), "BOTTOMLEFT", 0, -5);
+            below:SetPoint("TOPLEFT", getglobal("PlexusClickSetButton"..id), "BOTTOMLEFT", 0, -5)
         end
     end
 end
 
 function PlexusClickSetsFrame_Resize()
-    local k=0;
+    local k=0
     local id
     for id=1, 8 do
         local argF = getglobal("PlexusClickSetButton"..id.."Arg")
         if(argF:IsVisible()) then
-            k = k + argF:GetHeight() + 1;
+            k = k + argF:GetHeight() + 1
         end
     end
-    local height = 340 + k;
+    local height = 340 + k
     if(height > 450) then
-        PlexusClickSetsFrame:SetHeight(height);
+        PlexusClickSetsFrame:SetHeight(height)
     else
-        PlexusClickSetsFrame:SetHeight(450);
+        PlexusClickSetsFrame:SetHeight(450)
     end
 end
 
@@ -123,87 +136,95 @@ function PlexusClickSetsFrame_TypeUpdateAll()
     for i=1,8 do
         PlexusClickSetsFrame_TypeUpdate(i)
     end
-    PlexusClickSetsFrame_Resize();
+    PlexusClickSetsFrame_Resize()
 end
 
 function PlexusClickSets_Type_OnClick(self)
-    UIDropDownMenu_SetSelectedValue(self.owner, self.value);
+    UIDropDownMenu_SetSelectedValue(self.owner, self.value)
     local id=self.owner:GetParent():GetID()
     PlexusClickSetsFrame_TypeUpdate(id)
     PlexusClickSetsFrame_Resize()
 end
 
 local function ddname(str)
-    return " = "..str.." = ";
+    return " = "..str.." = "
 end
 
 function PlexusClickSetButton_TypeDropDown_Initialize(self)
-    local info;
-
+    local info
+    local spec
     local _, c = UnitClass("player")
-    local spec = GetSpecialization()
-    local list = PlexusClickSets_SpellList[c];
+    if IsRetailWow() then
+        spec = GetSpecialization()
+    end
+    if IsClassicWow() then
+        spec = 0
+    end
+    if IsTBCWow() then
+        spec = 0
+    end
+    local list = PlexusClickSets_SpellList[c]
     if(list) then
         for spellId, defSpec in pairs(list) do
             --ReplacingTalentSpell IsKnown = false, Replaced IsKnown = true
             if PlexusClickSets_MayHaveSpell(defSpec, spec) then
                 local name, _, icon = GetSpellInfo(spellId)
                 if name ~= nil then
-                    info = {};
+                    info = {}
                     info.text = name
-                    info.icon = icon;
-                    info.owner = self;
-                    info.func = PlexusClickSets_Type_OnClick;
+                    info.icon = icon
+                    info.owner = self
+                    info.func = PlexusClickSets_Type_OnClick
                     info.value = "spellId:"..spellId
                     info.tooltipFunc = function(self, tip, arg1) tip:SetSpellByID(arg1) end
                     info.tooltipFuncArg1 = spellId
-                    UIDropDownMenu_AddButton(info);
+                    UIDropDownMenu_AddButton(info)
                 end
             end
         end
     end
 
-    info = {};
-    info.text = ddname(TARGET);
-    info.owner = self;
-    info.func = PlexusClickSets_Type_OnClick;
+    info = {}
+    info.text = ddname(TARGET)
+    info.owner = self
+    info.func = PlexusClickSets_Type_OnClick
     info.value = "target"
-    UIDropDownMenu_AddButton(info);
+    UIDropDownMenu_AddButton(info)
 
-    info = {};
-    info.text = ddname(BINDING_NAME_ASSISTTARGET);
-    info.owner = self;
-    info.func = PlexusClickSets_Type_OnClick;
+    info = {}
+    info.text = ddname(BINDING_NAME_ASSISTTARGET)
+    info.owner = self
+    info.func = PlexusClickSets_Type_OnClick
     info.value = "assist"
-    UIDropDownMenu_AddButton(info);
+    UIDropDownMenu_AddButton(info)
 
-    info = {};
-    info.text = ddname(SKILL..NAME);
-    info.owner = self;
-    info.func = PlexusClickSets_Type_OnClick;
+    info = {}
+    info.text = ddname(SKILL..NAME)
+    info.owner = self
+    info.func = PlexusClickSets_Type_OnClick
     info.value = "spell"
-    UIDropDownMenu_AddButton(info);
+    UIDropDownMenu_AddButton(info)
 
-    --	info = {};
-    --	info.text = ddname(SOCIAL_LABEL);
-    --	info.owner = self;
-    --	info.func = PlexusClickSets_Type_OnClick;
+    --	info = {}
+    --	info.text = ddname(SOCIAL_LABEL)
+    --	info.owner = self
+    --	info.func = PlexusClickSets_Type_OnClick
     --	info.value = "menu"
-    --	UIDropDownMenu_AddButton(info);
+    --	UIDropDownMenu_AddButton(info)
 
-    info = {};
-    info.text = ddname(MACRO);
-    info.owner = self;
-    info.func = PlexusClickSets_Type_OnClick;
+    info = {}
+    info.text = ddname(MACRO)
+    info.owner = self
+    info.func = PlexusClickSets_Type_OnClick
     info.value = "macro"
-    UIDropDownMenu_AddButton(info);
+    UIDropDownMenu_AddButton(info)
 
-    info = {};
-    info.text = NONE; --ddname(NONE);
-    info.owner = self;
-    info.func = PlexusClickSets_Type_OnClick;
+    info = {}
+    info.text = NONE --ddname(NONE)
+    info.owner = self
+    info.func = PlexusClickSets_Type_OnClick
     info.value = "NONE"
-    UIDropDownMenu_AddButton(info);
+    UIDropDownMenu_AddButton(info)
 end
 
 function PlexusClickSetsFrame_GetTabSet(btn)
@@ -215,7 +236,7 @@ function PlexusClickSetsFrame_LoadSet(set)
     for i = 1, 8 do
         local dd = getglobal("PlexusClickSetButton"..i.."Type")
         local atts = set and set[PlexusClickSets_Modifiers[i]] or {}
-        UIDropDownMenu_Initialize(dd, PlexusClickSetButton_TypeDropDown_Initialize);
+        UIDropDownMenu_Initialize(dd, PlexusClickSetButton_TypeDropDown_Initialize)
         UIDropDownMenu_SetSelectedValue(dd, atts.type or "NONE")
         if(atts.arg) then
             getglobal("PlexusClickSetButton"..i.."Arg"):SetText(atts.arg)
@@ -224,13 +245,13 @@ function PlexusClickSetsFrame_LoadSet(set)
         end
     end
 
-    PlexusClickSetsFrame_TypeUpdateAll();
+    PlexusClickSetsFrame_TypeUpdateAll()
 end
 
 function PlexusClickSetsFrame_TabOnClick()
-    PlexusClickSetsFrame_LoadSet( PlexusClickSetsFrame_GetTabSet(PlexusClickSetsFrame.selectedTab) );
+    PlexusClickSetsFrame_LoadSet( PlexusClickSetsFrame_GetTabSet(PlexusClickSetsFrame.selectedTab) )
     if ( false and PlexusClickSetsFrame.selectedTab == 1 ) then
-        UIDropDownMenu_SetSelectedValue(PlexusClickSetButton1Type, "target");
+        UIDropDownMenu_SetSelectedValue(PlexusClickSetButton1Type, "target")
         UIDropDownMenu_DisableDropDown(PlexusClickSetButton1Type)
     else
         UIDropDownMenu_EnableDropDown(PlexusClickSetButton1Type)
@@ -267,11 +288,11 @@ function PlexusClickSetsFrame_SaveOnClick()
 end
 
 function PlexusClickSetsFrame_CancelOnClick()
-    PlexusClickSetsFrame_LoadSet( PlexusClickSetsFrame_GetTabSet(PlexusClickSetsFrame.selectedTab) );
+    PlexusClickSetsFrame_LoadSet( PlexusClickSetsFrame_GetTabSet(PlexusClickSetsFrame.selectedTab) )
 end
 
 function PlexusClickSetsFrame_CloseOnClick(self)
-    PlexusClickSetsFrame:Hide();
+    PlexusClickSetsFrame:Hide()
     local last = PlexusClickSetsFrame.lastFrame
     PlexusClickSetsFrame.lastFrame = nil
     if last then
@@ -293,12 +314,15 @@ PlexusClickSetsForTalents={}
 PlexusClickSetsFrame_Updates = {} --for other module insert
 
 function PlexusClickSetsFrame_GetCurrentSet()
-    local spec = GetSpecialization() or 0;
-    if PlexusClickSetsFrame:IsVisible() then
-        PlexusClickSetsFrameTalentText:SetFormattedText("%s: %s", SPECIALIZATION, select(2, GetSpecializationInfo(spec)) or UNKNOWN)
-    end
-    if PlexusClickSetsForTalents[spec]==nil then
-        PlexusClickSetsForTalents[spec] = PlexusClickSets_GetDefault(spec);
+    local spec = 0
+    if IsRetailWow() then
+        local spec = GetSpecialization() or 0
+        if PlexusClickSetsFrame:IsVisible() then
+            PlexusClickSetsFrameTalentText:SetFormattedText("%s: %s", SPECIALIZATION, select(2, GetSpecializationInfo(spec)) or UNKNOWN)
+        end
+        if PlexusClickSetsForTalents[spec]==nil then
+            PlexusClickSetsForTalents[spec] = PlexusClickSets_GetDefault(spec)
+        end
     end
     local set = PlexusClickSetsForTalents[spec]
     for i=1, PLEXUS_CLICK_SETS_BUTTONS+2 do set[tostring(i)] = set[tostring(i)] or {} end
@@ -319,9 +343,9 @@ function PlexusClickSetsFrame_UpdateAll(silent)
     end
 end
 
-SLASH_CLICKSET1 = "/clicksets";
-SLASH_CLICKSET2 = "/pcs";
-SLASH_CLICKSET3 = "/plexusclicksets";
+SLASH_CLICKSET1 = "/clicksets"
+SLASH_CLICKSET2 = "/pcs"
+SLASH_CLICKSET3 = "/plexusclicksets"
 SlashCmdList["CLICKSET"] = function(msg)
     if PlexusClickSetsFrame:IsVisible() then PlexusClickSetsFrame:Hide() else PlexusClickSetsFrame:Show() end
 end
